3c0c4d4a88ec2a133cb66eeff267b26998535a97
Kannan Balasubramanian <kannan-balasubramanian@users.noreply.github.com>
2022-05-09 11:21:04 +0530

    Initial commit

---
+++ src/webparts/spfxSortItems/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface ISpfxSortItemsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'SpfxSortItemsWebPartStrings' {
+  const strings: ISpfxSortItemsWebPartStrings;
+  export = strings;
+}

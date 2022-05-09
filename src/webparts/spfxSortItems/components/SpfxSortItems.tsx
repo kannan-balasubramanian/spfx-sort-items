67f1ad0f8dce31aa37bf010541260ff78f0f3529
Kannan Balasubramanian <kannan-balasubramanian@users.noreply.github.com>
2022-05-09 11:27:50 +0530

    Initial clean up

--- src/webparts/spfxSortItems/components/SpfxSortItems.tsx
+++ src/webparts/spfxSortItems/components/SpfxSortItems.tsx
@@ -19,9 +19,10 @@ export default class SpfxSortItems extends React.Component<ISpfxSortItemsProps, 
           <img alt="" src={isDarkTheme ? require('../assets/welcome-dark.png') : require('../assets/welcome-light.png')} className={styles.welcomeImage} />
           <h2>Well done, {escape(userDisplayName)}!</h2>
           <div>{environmentMessage}</div>
-          <div>Web part property value: <strong>{escape(description)}</strong></div>
+          {/* <div>Web part property value: <strong>{escape(description)}</strong></div> */}
+          <div><strong>{escape(description)}</strong></div>
         </div>
-        <div>
+        {/* <div>
           <h3>Welcome to SharePoint Framework!</h3>
           <p>
             The SharePoint Framework (SPFx) is a extensibility model for Microsoft Viva, Microsoft Teams and SharePoint. It's the easiest way to extend Microsoft 365 with automatic Single Sign On, automatic hosting and industry standard tooling.
@@ -36,7 +37,7 @@ export default class SpfxSortItems extends React.Component<ISpfxSortItemsProps, 
             <li><a href="https://aka.ms/spfx-yeoman-api" target="_blank">SharePoint Framework API reference</a></li>
             <li><a href="https://aka.ms/m365pnp" target="_blank">Microsoft 365 Developer Community</a></li>
           </ul>
-        </div>
+        </div> */}
       </section>
     );
   }
